8d8229fc518b344b80ccaf17bcc24b25a1a0a636
shanin18 <shamimhosan02@gmail.com>
2026-09-16 15:32:24 +0600

    fix imports

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,16 +1,14 @@
 import { SiteHeader } from "@/components/site-header";
 import { MobileNav } from "@/components/mobile-nav";
 import { SiteFooter } from "@/components/site-footer";
-import {
-  Hero,
-  About,
-  AcademicGuidance,
-  HowGapWorks,
-  Testimonials,
-  NewsFeed,
-  Partners,
-  FinalCta,
-} from "@/components/sections/FinalCta";
+import { Hero } from "@/components/home-sections/Hero";
+import { About } from "@/components/home-sections/About";
+import { AcademicGuidance } from "@/components/home-sections/AcademicGuidance";
+import { HowGapWorks } from "@/components/home-sections/HowGapWorks";
+import { Testimonials } from "@/components/home-sections/Testimonials";
+import { NewsFeed } from "@/components/home-sections/NewsFeed";
+import { Partners } from "@/components/home-sections/Partners";
+import { FinalCta } from "@/components/home-sections/FinalCta";
 import { getPublishedNews, getTestimonials } from "@/lib/cms-queries";
 
 export default async function HomePage() {
