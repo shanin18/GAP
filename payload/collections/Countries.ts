2360dbdcb632671b80667b8272aadc6fd5d2b914
shanin18 <shamimhosan02@gmail.com>
2026-09-20 21:02:09 +0600

    working on build errors

--- payload/collections/Countries.ts
+++ payload/collections/Countries.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from 'payload';
-import { revalidateAfterChange, revalidateAfterDelete } from '../hooks/revalidate';
 import { isAdmin, isLoggedIn, publicRead } from '../access';
+import { revalidateAfterChange, revalidateAfterDelete } from '../hooks/revalidate';
 import { slugFrom } from '../hooks/slug';
 import { urlOrPath } from '../hooks/validators';
 
